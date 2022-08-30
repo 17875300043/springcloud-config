3bd853a808ef1ff7923a982878055e2209cced2a
lzp <1582417567@qq.com>
2022-08-30 09:28:22 +0800

    删除了cloud-gateway-gateway9527

--- cloud-gateway-gateway9527/src/test/java/demo.java
+++
@@ -1,8 +0,0 @@
-import java.time.ZonedDateTime;
-
-public class demo {
-    public static void main(String[] args) {
-        ZonedDateTime now = ZonedDateTime.now();
-        System.out.println(now);
-    }
-}
